611daa3f3fe1edec404c3f3ba77e5072896fe37b
Lucas Solium Dev <lucas.develop.prog@gmail.com>
2023-09-20 19:48:10 -0300

    feat: ISensorFields interface created

---
+++ src/iot-box/domain/entities/interfaces/sensor-fields.interface.ts
@@ -0,0 +1,13 @@
+export interface ISensorFields {
+  heat: number;
+  acceleration: number;
+
+  umity: number;
+  ultrasonic: number;
+  infraRed: number;
+  weight: number;
+  gas: number;
+  flux: number;
+  magnetic: number;
+  soundIntensity: number;
+}
