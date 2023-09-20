adfc72817898fe3e9fd0d9e6b8cdf12260c601da
Lucas Solium Dev <lucas.develop.prog@gmail.com>
2023-09-20 19:52:55 -0300

    feat: IotBoxEntity created and implemented

--- src/iot-box/domain/entities/iot-box.entity.ts
+++ src/iot-box/domain/entities/iot-box.entity.ts
@@ -1,1 +1,20 @@
-export class IotBox {}
+import { Entity } from '@/shared/domain/entities/entity';
+import { ISensorFields } from './interfaces/sensor-fields.interface';
+import { IGPS } from './interfaces/gps.interface';
+
+export interface IotBoxProps {
+  id: string;
+  customerId?: string;
+  date: Date;
+  hourTime: Date;
+  battery: number;
+  gps: IGPS;
+  sensors: Partial<ISensorFields>;
+}
+
+export class IotBox extends Entity<IotBoxProps> {
+  public props: IotBoxProps;
+  constructor(props: IotBoxProps) {
+    super(props);
+  }
+}
