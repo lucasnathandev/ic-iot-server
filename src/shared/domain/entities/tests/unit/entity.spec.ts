4a52de46e7ad511213da15ea157d7e6c8261166f
Lucas Solium Dev <lucas.develop.prog@gmail.com>
2023-09-30 19:24:32 -0300

    test: ensure abstract class Entity instance works properly

--- src/shared/domain/entities/tests/unit/entity.spec.ts
+++ src/shared/domain/entities/tests/unit/entity.spec.ts
@@ -7,10 +7,21 @@ interface StubProps {
 class StubEntity extends Entity<StubProps> {}
 
 describe('Entity abstract class unit tests', () => {
+  const props: StubProps = { name: 'Lucas', age: 27 };
+  const sut = new StubEntity(props);
+
   it('should have name and age props', () => {
-    const props: StubProps = { name: 'Lucas', age: 27 };
-    const sut = new StubEntity(props);
     expect(sut.props.name).toBeDefined();
     expect(sut.props.age).toBeDefined();
+  });
+
+  it('should NOT have these prop values', () => {
+    expect(sut.props.name).not.toBe('John');
+    expect(sut.props.age).not.toBe(20);
+  });
+
+  it('should have correct prop values', () => {
+    expect(sut.props.name).toBe(props.name);
+    expect(sut.props.age).toBe(props.age);
   });
 });
