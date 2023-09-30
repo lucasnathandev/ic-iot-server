68f12ed29b0f89b129712334a1dcf514c8289356
Lucas Solium Dev <lucas.develop.prog@gmail.com>
2023-09-30 17:57:09 -0300

    feat: Abstract class Entity updated

--- src/shared/domain/entities/entity.ts
+++ src/shared/domain/entities/entity.ts
@@ -1,6 +1,12 @@
 export abstract class Entity<Props> {
   public readonly props: Props;
-  constructor(props: Props) {
-    this.props = props;
+  public readonly id: string;
+  constructor(props: Props, id?: string) {
+    Object.entries(props).forEach((prop) => (this[prop[0]] = prop[1]));
+    this.id =
+      id ||
+      Array.from({ length: 32 })
+        .map(() => String.fromCharCode(Math.floor(Math.random() * 36 + 65)))
+        .join();
   }
 }
