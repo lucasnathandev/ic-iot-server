f9bd91dac48c33d2d1a1762b35448b8aa5f8a2e9
Lucas Solium Dev <lucas.develop.prog@gmail.com>
2023-10-04 22:23:19 -0300

    feat: CustomerModule added to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { CustomerModule } from './customer/customer.module';
 
 @Module({
-  imports: [IotBoxModule, ConfigModule.forRoot()],
+  imports: [IotBoxModule, ConfigModule.forRoot(), CustomerModule],
   controllers: [AppController],
   providers: [AppService],
 })
