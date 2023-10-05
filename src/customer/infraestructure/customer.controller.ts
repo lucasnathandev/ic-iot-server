4f4082e1e4d757fccd2a1b40304ab1a14162769e
Lucas Solium Dev <lucas.develop.prog@gmail.com>
2023-10-04 22:21:13 -0300

    feat: Customer resoures created

---
+++ src/customer/infraestructure/customer.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { UpdateCustomerDto } from './infraestructure/dto/update-customer.dto';
+import { CreateCustomerDto } from './infraestructure/dto/create-customer.dto';
+import { CustomerService } from './customer.service';
+
+@Controller('customer')
+export class CustomerController {
+  constructor(private readonly customerService: CustomerService) {}
+
+  @Post()
+  create(@Body() createCustomerDto: CreateCustomerDto) {
+    return this.customerService.create(createCustomerDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.customerService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.customerService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateCustomerDto: UpdateCustomerDto,
+  ) {
+    return this.customerService.update(+id, updateCustomerDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.customerService.remove(+id);
+  }
+}
