f530d68f08ab913c3c5dcadaca575cde0f499b60
Lucas Solium Dev <lucas.develop.prog@gmail.com>
2023-10-06 12:32:39 -0300

    test: ensure CustomerEntity methods works fine

--- src/customer/domain/entites/test/unit/customer-entity.spec.ts
+++ src/customer/domain/entites/test/unit/customer-entity.spec.ts
@@ -1,5 +1,6 @@
 import { CPF } from 'src/shared/application/lib/CPF';
 import { CustomerEntity } from '../../customer.entity';
+import { IotBoxEntity } from 'src/iot-box/domain/entities/iot-box.entity';
 
 describe('CustomerEntity unit tests', () => {
   let sut: CustomerEntity;
@@ -34,5 +35,27 @@ describe('CustomerEntity unit tests', () => {
     expect(sut.age).toBe(30);
     expect(sut.cpf).toBe(cpf);
     expect(sut.boxes).toStrictEqual([]);
+  });
+
+  it('should run methods correctly', () => {
+    sut = new CustomerEntity({
+      name: 'Jane Doe',
+      age: 27,
+      cpf: new CPF().generateRandomCpf('RJ'),
+      boxes: [],
+    });
+
+    const box = new IotBoxEntity({
+      battery: 1,
+      customerId: sut.id,
+      date: new Date(),
+      hourTime: '12:00',
+      sensors: { gps: { latitude: 10.02, longitude: -20.2 } },
+    });
+
+    expect(sut.boxes.length).toBe(0);
+    sut.acquireBox(box);
+    expect(sut.boxes.length).toBe(1);
+    expect(sut.boxes[0]).toStrictEqual(box);
   });
 });
