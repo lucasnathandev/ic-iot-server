fbd96ccbd24230be10e4f9d307abfc22e27194dc
Solium Dev <lucas.develop.prog@gmail.com>
2023-10-20 18:19:22 -0300

    refactor: Repository interface updated

--- src/shared/domain/repository/repository.ts
+++ src/shared/domain/repository/repository.ts
@@ -1,6 +1,6 @@
 export interface Repository<T> {
   get(id: string): Promise<T>;
   save(entity: T): Promise<void>;
-  update(entity: T): Promise<void>;
+  update(id: string, data: T | Partial<T>): Promise<void>;
   delete(id: string): Promise<void>;
 }
