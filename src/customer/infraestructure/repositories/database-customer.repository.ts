e797d1b69f73e3101377b521485b0db1076b1296
Solium Dev <lucas.develop.prog@gmail.com>
2023-10-20 19:09:48 -0300

    feat: CustomerRepositoryDatabase added without implementing

---
+++ src/customer/infraestructure/repositories/database-customer.repository.ts
@@ -0,0 +1,23 @@
+import { CustomerEntity } from 'src/customer/domain/entites/customer.entity';
+import { CustomerRepository } from 'src/customer/domain/repositories/customer.repository';
+
+export class CustomerRepositoryDatabase implements CustomerRepository {
+  findByEmail(email: string) {
+    throw new Error('Method not implemented.');
+  }
+  findByCpf(cpf: string) {
+    throw new Error('Method not implemented.');
+  }
+  get(id: string): Promise<CustomerEntity> {
+    throw new Error('Method not implemented.');
+  }
+  save(entity: CustomerEntity): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  update(entity: CustomerEntity | Partial<CustomerEntity>): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  delete(id: string): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+}
