53676a66ff60fae0e4e0c1bad070279f570feafa
Solium Dev <lucas.develop.prog@gmail.com>
2023-10-24 17:11:13 -0300

    feat: IotBoxEntity added new methods and properties

---
+++ src/iot-box/domain/entities/interfaces/enum.battery-status.ts
@@ -0,0 +1,5 @@
+export enum BatteryStatus {
+  Low = 0.2,
+  Medium = 0.5,
+  High = 1,
+}
--- src/iot-box/domain/entities/interfaces/iot-box-methods.interface.ts
+++ src/iot-box/domain/entities/interfaces/iot-box-methods.interface.ts
@@ -1,9 +1,15 @@
+import { BatteryStatus } from './enum.battery-status';
 import { IGPS } from './gps.interface';
 import { ISensorFields } from './sensor-fields.interface';
 
 export interface IotBoxMethods {
   setBoxOwnerId(id: string): void;
   unbindOwnerCustomer(): void;
+  updateBatteryStatus(battery: number): void;
+  checkBatteryStatus(): BatteryStatus;
   updateGPSLocation(coordinates: IGPS): void;
   updateAllSensors(data: Partial<ISensorFields>): void;
+  getBatteryStatus(): BatteryStatus;
+  inactivateBox(): void;
+  activateBox(): void;
 }
