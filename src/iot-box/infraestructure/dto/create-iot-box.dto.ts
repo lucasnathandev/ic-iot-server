577d0d88ddeb1683d248d236a0a9f277fe0b2e49
Solium Dev <lucas.develop.prog@gmail.com>
2023-10-24 17:13:52 -0300

    feat: IotBoxRepository added, implemented and tested

--- src/iot-box/infraestructure/dto/create-iot-box.dto.ts
+++ src/iot-box/infraestructure/dto/create-iot-box.dto.ts
@@ -1,1 +1,8 @@
-export class CreateIotBoxDto {}
+import { BatteryStatus } from 'src/iot-box/domain/entities/interfaces/enum.battery-status';
+import { IotBoxProps } from 'src/iot-box/domain/entities/interfaces/iot-box-props.interface';
+import { ISensorFields } from 'src/iot-box/domain/entities/interfaces/sensor-fields.interface';
+
+export class CreateIotBoxDto implements IotBoxProps {
+  name: string;
+  sensors: ISensorFields;
+}
