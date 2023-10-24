daaa7f0831c359bc740378b63f4c62b5020d3c11
Solium Dev <lucas.develop.prog@gmail.com>
2023-10-24 18:10:52 -0300

    refactor: CreateIotBoxDto updated

--- src/iot-box/infraestructure/dto/create-iot-box.dto.ts
+++ src/iot-box/infraestructure/dto/create-iot-box.dto.ts
@@ -1,4 +1,3 @@
-import { BatteryStatus } from 'src/iot-box/domain/entities/interfaces/enum.battery-status';
 import { IotBoxProps } from 'src/iot-box/domain/entities/interfaces/iot-box-props.interface';
 import { ISensorFields } from 'src/iot-box/domain/entities/interfaces/sensor-fields.interface';
 
