1f540246e0803f6177063246815b941833e2b15e
Solium Dev <lucas.develop.prog@gmail.com>
2023-10-24 17:07:32 -0300

    feat: new methods added and tested

--- src/admin/application/service/test/admin-application-service.spec.ts
+++ src/admin/application/service/test/admin-application-service.spec.ts
@@ -11,17 +11,38 @@ describe('AdminApplicationService integration tests', () => {
   });
 
   it('should run methods corretly', async () => {
-    const stubAdmin = new AdminEntity({
-      name: 'I am admin',
-      age: 35,
-      cpf: '12345678901',
-      email: 'iamadmin@gmail.com',
-      password: 'iamadminpass',
-      isActive: true,
-    });
+    const stubAdmin = new AdminEntity(
+      {
+        name: 'I am admin',
+        age: 35,
+        cpf: '12345678901',
+        email: 'iamadmin@gmail.com',
+        password: 'iamadminpass',
+        isActive: true,
+      },
+      'fakeadminid',
+    );
 
     await sut.createAdmin(stubAdmin);
     expect(await sut.findAdmin(stubAdmin.id)).toStrictEqual(stubAdmin);
-    await expect(sut.findAdmin('fakeadminid')).rejects.toThrow();
+    await expect(sut.findAdmin('anyadminid')).rejects.toThrow();
+    const allAdminList = await sut.allAdminList();
+    const foundAdmin = await sut.findAdmin(stubAdmin.id);
+    const foundAdminBySearchEmail = await sut.searchAdmin({
+      email: stubAdmin.email,
+    });
+    const foundAdminBySearchCPF = await sut.searchAdmin({
+      cpf: stubAdmin.cpf,
+    });
+    expect(allAdminList).toHaveLength(1);
+    expect(foundAdmin).toStrictEqual(stubAdmin);
+    expect(foundAdminBySearchEmail).toStrictEqual(stubAdmin);
+    expect(foundAdminBySearchCPF).toStrictEqual(stubAdmin);
+
+    await sut.updateAdmin(stubAdmin.id, { password: '123456' });
+    expect(await sut.findAdmin(stubAdmin.id)).toContain({ password: '123456' });
+    await sut.deleteAdmin(stubAdmin.id);
+    await expect(sut.findAdmin(stubAdmin.id)).rejects.toThrow();
+    expect(await sut.allActiveAdminList()).toHaveLength(0);
   });
 });
