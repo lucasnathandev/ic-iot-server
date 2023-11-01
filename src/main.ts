a7f0727f59c5ca0abd4cdd579e0580171a4c7069
Solium Dev <lucas.develop.prog@gmail.com>
2023-11-01 14:51:31 -0300

    feat: Swagger added and setted up

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './shared/infra/app.module';
 import { Logger } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const logger = new Logger(bootstrap.name);
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API Docs')
+    .setDescription('IC Iot API routes documentation')
+    .setVersion('0.8')
+    .addTag('api')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('', app, document);
+
   const port = process.env.PORT || 8000;
   await app.listen(port, () => logger.log(`Server listening on port ${port}`));
 }
